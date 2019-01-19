6122f108aa176d92fc4595080132e76e4e4beff8
MandolinMicke <andmika@gmail.com>
2019-01-19 17:01:44 +0100

    take care of email connection error

--- Python/mail_handler.py
+++ Python/mail_handler.py
@@ -26,42 +26,53 @@ SMTP_PORT   = 993
 # ------------------------------------------------
 
 def read_email_from_gmail(subject = None, user = 'tarendo'):
-    mail = imaplib.IMAP4_SSL(SMTP_SERVER)
-    mail.login(mc.EMAIL[user],mc.PWD[user])
-    mail.select('inbox')
+    while True:
+        mail = imaplib.IMAP4_SSL(SMTP_SERVER)
+        mail.login(mc.EMAIL[user],mc.PWD[user])
 
-    # Out: list of "folders" aka labels in gmail.
-    mail.select("inbox") # connect to inbox.
-
-    # Get all unread emails
-    if subject == None:
-        typ, msg_num = mail.search(None,'(UNSEEN)')
-    else:
-        typ, msg_num = mail.search(None,'(UNSEEN SUBJECT "' + subject + ' ")')
+        try:    
+            # Out: list of "folders" aka labels in gmail.
+            mail.select("inbox") # connect to inbox.
         
-    # check if a new mail is here and get the text
-    if msg_num[0].decode().split(' ')[0]:
-        rettext = []
-        for num in msg_num[0].decode().split(' '):
-            result, data = mail.fetch(num.encode(), '(RFC822)')
-            raw_email = data[0][1].decode()
-            email_message = email.message_from_string(raw_email)
-#            maintype = email_message.get_content_maintype()
-            mejltext = email_message.get_payload()
-            rettext.append(mejltext)
-    else:
-        rettext = None
-    mail.close()
+            # Get all unread emails
+            if subject == None:
+                typ, msg_num = mail.search(None,'(UNSEEN)')
+            else:
+                typ, msg_num = mail.search(None,'(UNSEEN SUBJECT "' + subject + ' ")')
+                
+            # check if a new mail is here and get the text
+            if msg_num[0].decode().split(' ')[0]:
+                rettext = []
+                for num in msg_num[0].decode().split(' '):
+                    result, data = mail.fetch(num.encode(), '(RFC822)')
+                    raw_email = data[0][1].decode()
+                    email_message = email.message_from_string(raw_email)
+                    mejltext = email_message.get_payload()
+                    rettext.append(mejltext)
+            else:
+                rettext = None
+            
+        except imaplib.abort:
+            continue
+        mail.logout()
+#        mail.close()
+        break
     return rettext
 
 def decodeMail(text):
     retcommands = {}
     for i in text:
         stringlist = i.split('\n')
-        for s in stringlist:
-            if any([x in s for x in sc.systemcommands]):
-                strs = s.strip().split(' ')
-                retcommands[strs[0]] = strs[1]
+        if stringlist[0].strip() == 'History':
+            retcommands = []
+            del(stringlist[0])
+            for s in stringlist:
+                retcommands.append(s.strip())
+        else:
+            for s in stringlist:
+                if any([x in s for x in sc.systemcommands]):
+                    strs = s.strip().split(' ')
+                    retcommands[strs[0]] = strs[1]
     return retcommands
 
 
@@ -82,7 +93,7 @@ def sendEmail(body,fromaddr,toaddr,subject):
         print('Something went wrong...')
 if __name__ == '__main__':
      text = read_email_from_gmail('GUI', 'tarendo')
-     
+#     print(text)
      if text != None:
          print(decodeMail(text))
 #    sendEmail('hej hopp','tarendo','system')
